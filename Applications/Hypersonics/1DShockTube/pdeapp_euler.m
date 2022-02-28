a0dd71ff3241dc0f6decf58045bf3f1e6c949d94
rloekvh <rloekvh@gmail.com>
2022-02-28 15:07:13 -0500

    updates to examples

--- Applications/Hypersonics/1DShockTube/pdeapp_euler.m
+++ Applications/Hypersonics/1DShockTube/pdeapp_euler.m
@@ -5,12 +5,12 @@ run(cdir(1:(ii+5)) + "/Installation/setpath.m");
 % initialize pde structure and mesh structure
 [pde,mesh] = initializeexasim();
 
-pde.mutationflag = 1;
-pde.mutationopts = {};
-pde.mutationopts{1} = "air_5";
-pde.mutationopts{2} = "ChemNonEq1T";
-pde.mutationopts{3} = "NASA-9";
-pde.mutationpath = "/Users/rloekvh/Mutationpp";
+% pde.mutationflag = 1;
+% pde.mutationopts = {};
+% pde.mutationopts{1} = "air_5";
+% pde.mutationopts{2} = "ChemNonEq1T";
+% pde.mutationopts{3} = "NASA-9";
+% pde.mutationpath = "/Users/rloekvh/Mutationpp";
 
 pde.cpucompiler="/opt/homebrew/Cellar/llvm@12/12.0.1_1/bin/clang++";
 % pde.cpuappflags = "-arch x86_64";
@@ -19,28 +19,28 @@ pde.cpucompiler="/opt/homebrew/Cellar/llvm@12/12.0.1_1/bin/clang++";
 
 % Define a PDE model: governing equations, initial solutions, and boundary conditions
 pde.model = "ModelC";          % ModelC, ModelD, ModelW
-pde.modelfile = "pdemodel_inflow";    % name of a file defining the PDE model
+pde.modelfile = "pdemodel_euler";    % name of a file defining the PDE model
 
 % Choose computing platform and set number of processors
 %pde.platform = "gpu";         % choose this option if NVIDIA GPUs are available
 pde.mpiprocs = 1;              % number of MPI processors
-
+% TODO: THINK ABOUT MACH NUMBER, MAKE SURE SCALING IS CORRECT!!!
 % Set discretization parameters, physical parameters, and solver parameters
 pde.porder = 1;          % polynomial degree
-pde.torder = 1;          % time-stepping order of accuracy
-pde.nstage = 1;          % time-stepping number of stages
-nt = 10;
-pde.dt = 1e-3*ones(1,nt);   % time step sizes
-pde.saveSolFreq = 10000;
-% pde.soltime = 1:pde.saveSolFreq:length(pde.dt); % steps at which solution are collected
+% pde.torder = 2;          % time-stepping order of accuracy
+% pde.nstage = 2;          % time-stepping number of stages
+% nt = 100000;
+% pde.dt = 1e-4*ones(1,nt);   % time step sizes
+% pde.saveSolFreq = 10000;
+pde.soltime = pde.saveSolFreq:pde.saveSolFreq:length(pde.dt); % steps at which solution are collected
 
 %Solver params
-pde.linearsolveriter = 40;
-pde.GMRESrestart = 20;
-pde.linearsolvertol = 1e-1;
-pde.matvectol = 1e-1;
-pde.NLiter = 5;
-pde.RBdim = 5;
+% pde.linearsolveriter = 40;
+% pde.linearsolvertol = 1e-1;
+% pde.GMRESrestart = 20;
+% pde.matvectol = 1e-2;
+% pde.NLiter = 10;
+% pde.RBdim = 10;
 
 % rhoL = [0 0 0.000936356 0.000248905 0];
 % rhouL = 4.74105;
@@ -48,36 +48,48 @@ pde.RBdim = 5;
 % rhoR = [0 0 0.00691678 0.00183864 0];
 % rhouR = 4.74544;
 % rhoER = 72810.4;
-rho_pre = [0 0 0 0.000911061 0.000242181 ];
+% rho_pre = [0 0 0 0.000911061 0.000242181 ];
+rho_pre = 0.0012;
 rhou_pre = 4.61296;
 rhoE_pre = 9128.09;
+u_pre = 4000.0;
 
-rho_post = [0 0 0 0.00672992 0.00178897 ];
+% rho_post = [0 0 0 0.00672992 0.00178897 ];
+rho_post = 0.0085;
 rhou_post = 4.61724;
 rhoE_post = 52157.2;
+u_post = 542.0;
 
-rho_inf = sum(rho_post);
-u_inf = rhou_post/rho_inf;
-
-% pde.physicsparam = [rho_pre(:)', rhou_pre, rhoE_pre, rho_post(:)', rhou_post, rhoE_post];
-pde.physicsparam = [rho_pre(:)'/rho_inf, rhou_pre/(rho_inf * u_inf), rhoE_pre/(rho_inf * u_inf * u_inf), rho_post(:)'/rho_inf, rhou_post/(rho_inf*u_inf), rhoE_post/(rho_inf * u_inf * u_inf)];
-
-pde.externalparam = [rho_inf, u_inf];
+rho_inf = rho_post;
+u_inf = u_post;
+%%
+pde.physicsparam = [rho_pre(:)'/rho_inf,...
+                    rhou_pre/(rho_inf*u_inf),...
+                    rhoE_pre/(rho_inf*u_inf^2),...
+                    rho_post(:)'/rho_inf,...
+                    rhou_post/(rho_inf*u_inf),...
+                    rhoE_post/(rho_inf*u_inf^2)/3];
+% pde.physicsparam = [rho_pre(:)',...
+%                     rhou_pre,...
+%                     rhoE_pre,...
+%                     rho_post(:)',...
+%                     rhou_post,...
+%                     rhoE_post];
 % pde.physicsparam = [rhoL(:)', rhouL, rhoEL,    rhoR(:)',  rhouR,  rhoER];
                 %    1:ns    ns+1   ns+2   ns+3:2*ns+2  2*ns+3  2*ns+4
 % pde.tau = [0.1, 0.1, 0.1, 0.1, 0.1, 10, 50000];           % DG stabilization parameter
-pde.tau = 0.02;
-pde.dae_alpha = 0;
-pde.dae_beta = 1;
+pde.tau = 15;
+% pde.dae_alpha = 0;
+% pde.dae_beta = 1;
 % create a grid of 8 by 8 on the unit square
-nDiv = 2^9;
+nDiv = 2^12;
 
 [mesh.p,mesh.t] = linemesh(nDiv-1);
-a = 0; b = 2;
+a = 0; b = 1;
 mesh.p = a + (b-a)*mesh.p;
 mesh.p = loginc(mesh.p, 10);
 % expressions for domain boundaries
-mesh.boundaryexpr = {@(p) abs(p(1,:)-a)<1e-16, @(p) abs(p(1,:) - b)<1e-8}; %TODO: double check boundaries
+mesh.boundaryexpr = {@(p) abs(p(1,:)-a)<1e-8, @(p) abs(p(1,:) - b)<1e-8}; %TODO: double check boundaries
 mesh.boundarycondition = [1;2]; % Set boundary condition for each boundary
 
 %% call exasim to generate and run C++ code to solve the PDE model
@@ -92,11 +104,11 @@ pde = setcompilers(pde);
 % generate source codes and store them in app folder
 gencode(pde);
 cd("app");
-eval('!./fixargsMPP_Mac.sh');
+% eval('!./fixargsMPP_Mac.sh');
 % eval('!./movefiles.sh');
 % eval('!cp opuInituMPP.cpp opuInitu.cpp');  
-eval('!cp opuInitwdgMPP.cpp opuInitwdg.cpp');
-eval('!cp opuSourcewMPP.cpp opuSourcew.cpp');
+% eval('!cp opuInitwdgMPP.cpp opuInitwdg.cpp');
+% eval('!cp opuSourcewMPP.cpp opuSourcew.cpp');
 % eval('!cp opuFbou_MPP.cpp opuFbou.cpp');
 % eval('!cp opuFlux_MPP.cpp opuFlux.cpp');
 % eval('!cp opuSource_MPP.cpp opuSource.cpp');
@@ -110,27 +122,30 @@ runstr = runcode(pde);
 
 %% plot solution
 % sol = fetchsolution(pde,master,dmd, 'dataout');
-ti = nt;
-sol = getsolution(['dataout/out_t' num2str(ti)],dmd,master.npe);
+% for ti = pde.soltime
+% ti = nt;
+% sol = getsolution(['dataout/out_t' num2str(ti)],dmd,master.npe);
+sol = getsolution(['dataout/out'],dmd,master.npe);
+nspecies=1;
 dgnodes = createdgnodes(mesh.p,mesh.t,mesh.f,mesh.curvedboundary,mesh.curvedboundaryexpr,pde.porder);    
-for i = 1:5    
-    u = sol(:,i,:);
-    subplot(1,3,1)
-    hold on
-    plot(dgnodes(:),u(:),'LineWidth',1.3)
+for i = 1:nspecies 
+    rho = sol(:,i,:);
+%     subplot(1,3,1)
+%     hold on
+%     plot(dgnodes(:),u(:),'LineWidth',1.3)
 end
-u = sol(:,6,:);
-subplot(1,3,2)
-plot(dgnodes(:),u(:),'LineWidth',1.3);
+rhou = sol(:,nspecies+1,:);
+% subplot(1,3,2)
+% plot(dgnodes(:),u(:),'LineWidth',1.3);
 
-u = sol(:,7,:);
-subplot(1,3,3)
-plot(dgnodes(:), u(:),'LineWidth',1.3)
+rhoE = sol(:,nspecies+2,:);
 
-% solw = getsolution(['dataout/out_wdg_t' num2str(ti)],dmd,master.npe);
-% w = solw(:,6,:);
-% figure(2);
-% plot(dgnodes(:),w(:))
+u = rhou./rho;
+% subplot(1,3,3)
+plot(dgnodes(:), u(:),'LineWidth',1.3); drawnow
+% end
+% plot
+
 
 
         
--- Applications/Hypersonics/1DShockTube/pdemodel_euler.m
+++ Applications/Hypersonics/1DShockTube/pdemodel_euler.m
@@ -1,27 +1,25 @@
  
 function pde = pdemodel_inflow
-    pde.mass = @mass;
+%     pde.mass = @mass;
     pde.flux = @flux;
     pde.source = @source;
     pde.fbou = @fbou;
     pde.ubou = @ubou;
     pde.initu = @initu;
-    pde.sourcew = @sourcew;
-    pde.initw = @initw;
 end
 
 function m = mass(u, q, w, v, x, t, mu, eta)
-    nspecies = 5;
+    nspecies = 1;
     m = sym(ones(nspecies + 2, 1));
-    m = sym([1.0; 1.0; 1.0; 1.0; 1.0]); 
+%     m = sym([1.0; 1.0; 1.0; 1.0; 1.0]); 
 end
 
 function f = flux(u, q, w, v, x, t, mu, eta)
 
-    nspecies = 5;
+    nspecies = 1;
 
     f = sym(zeros(nspecies + 2,1));
-    rho_i = sym(zeros(5,1));
+    rho_i = sym(zeros(1,1));
     rho = sym(0);
 
     % Conservative Variables
@@ -35,15 +33,14 @@ function f = flux(u, q, w, v, x, t, mu, eta)
 
     rhoinv = 1.0 / rho;
     u = rhou * rhoinv; %velocity
-    E = rhoE*rhoinv; %energy
+    E = rhoE * rhoinv; %energy
 
     % Mutation outputs
-	rho_inf = 0.0085;
-    u_inf = 542.0;
-    rhoe_inf = rho_inf * u_inf^2;
 
-    p = w(nspecies + 1)/rhoe_inf; %pressure
-
+%     p = w(nspecies + 1); %pressure
+    gamma = 1.4;
+    gammam1 = gamma-1;
+    p = gammam1 * (rhoE - 0.5 * rho * u^2);
     H = E+p*rhoinv; %enthalpy
 
     % Fluxes
@@ -57,10 +54,10 @@ end
 
 function s = source(u, q, w, v, x, t, mu, eta)
     
-    nspecies = 5;
+    nspecies = 1;
 
     s = sym(zeros(nspecies + 2,1));
-    omega_i = sym(zeros(nspecies,1));
+%     omega_i = sym(zeros(nspecies,1));
 %     rho_i = sym(zeros(5,1));
 %     rho = 0;
 % 
@@ -72,31 +69,27 @@ function s = source(u, q, w, v, x, t, mu, eta)
 % 
 %     rhou = u(nspecies+1);
 %     rhoE = u(nspecies+2);
-
+% 
 %     rhoinv = 1/ rho;
 %     u = rhou * rhoinv; %velocity
 %     E = rhoE*rhoinv; %energy
 % 
-    % Mutation outputs
-    for ispecies = 1:nspecies
-        omega_i(ispecies) = w(ispecies);
-    end
+%     % Mutation outputs
+%     for ispecies = 1:nspecies
+%         omega_i(ispecies) = w(ispecies);
+%     end
 %     p = w(nspecies + 1); %pressure
 % 
 %     H = E+p*rhoinv; %enthalpy
-
-    rho_inf = 0.0085;
-%     u_inf = 542.0;
-%     rhoe_inf = rho_inf * u_inf^2;
-
-    % Sources
-    for ispecies = 1:nspecies
-        s(ispecies) = omega_i(ispecies)/rho_inf;
-    end
+% 
+%     % Sources
+%     for ispecies = 1:nspecies
+%         s(ispecies) = omega_i(ispecies);
+%     end
 end
 
 function ub = ubou(u, q, w, v, x, t, mu, eta, uhat, n, tau)
-    nspecies = 5;
+    nspecies = 1;
     ub = sym(zeros(nspecies+2, 2));
 
 %     rhoL = mu(1:nspecies);
@@ -109,25 +102,30 @@ function ub = ubou(u, q, w, v, x, t, mu, eta, uhat, n, tau)
 
 %     ub(:,1) = [rhoL(:); rhouL; rhoEL];
     
-    ub(:,1) = mu(8:14);
+%     ub(:,1) = mu(8:14);
+%     rho_post = mu(nspecies + 3: 2*nspecies + 2);
+%     rhou_post = mu(2*nspecies + 3);
+%     rhoE_post = mu(2*nspecies + 4);
+    ub(:,1) = mu(nspecies + 3: 2*nspecies + 4);
 %     ub(:,1) = u(:);
     ub(:,2) = u(:);
 end
 
 function fb = fbou(u, q, w, v, x, t, mu, eta, uhat, n, tau)
-    nspecies = 5;
+    nspecies = 1;
     fb = sym(zeros(nspecies+2, 2));
 
     f = flux(u, q, w, v, x, t, mu, eta);
-    finflow = flux(mu(8:14), q, w, v, x, t, mu, eta);
-    fb(:,1) = finflow*n(1) + tau.*(mu(8:14)-uhat);
+    uinflow = mu(nspecies + 3: 2*nspecies + 4);
+    finflow = flux(uinflow, q, w, v, x, t, mu, eta);
+    fb(:,1) = finflow*n(1) + tau(1)*(uinflow-uhat);
 %     fb(:,1) = f*n(1) + tau.*(u-uhat);
-    fb(:,2) = f*n(1) + tau.*(u-uhat);
+    fb(:,2) = f*n(1) + tau(1)*(u-uhat);
 end
 
 function u0 = initu(x, mu, eta)
     % To be manually modified
-    nspecies = 5;
+    nspecies = 1;
     u0 = sym(zeros(nspecies+2,1));
 
     rho_pre = mu(1:nspecies);
@@ -137,49 +135,49 @@ function u0 = initu(x, mu, eta)
     rho_post = mu(nspecies+3:2*nspecies+2);
     rhou_post = mu(2*nspecies+3);
     rhoE_post = mu(2*nspecies+4);
-    u0 = mu(1:nspecies + 2);
+    u0 = mu(1:nspecies+2);
 %     for i = 1:nspecies
 %         u0(i) = smoothstep(x, rho_post(i), rho_pre(i), 1/1000);
 %     end
-%     u0(1) = smoothstep_up(x, rho_post(1), rho_pre(1), 1/10);
-%     u0(2) = smoothstep_up(x, rho_post(2), rho_pre(2), 1/10);
-%     u0(3) = smoothstep_up(x, rho_post(3), rho_pre(3), 1/10);
-%     u0(4) = smoothstep_down(x, rho_post(4), rho_pre(4), 1/10);
-%     u0(5) = smoothstep_down(x, rho_post(5), rho_pre(5), 1/10);
-%     u0(nspecies+1) = smoothstep_down(x, rhou_post, rhou_pre, 1/10);
-%     u0(nspecies+2) = smoothstep_down(x, rhoE_post, rhoE_pre, 1/10);
+%     u0(1) = smoothstep_up(x, rho_post(1), rho_pre(1), 1/1000);
+%     u0(2) = smoothstep_up(x, rho_post(2), rho_pre(2), 1/100);
+%     u0(3) = smoothstep_up(x, rho_post(3), rho_pre(3), 1/100);
+%     u0(4) = smoothstep_down(x, rho_post(4), rho_pre(4), 1/100);
+%     u0(5) = smoothstep_down(x, rho_post(5), rho_pre(5), 1/100);
+%     u0(nspecies+1) = smoothstep_down(x, rhou_post, rhou_pre, 1/1000);
+%     u0(nspecies+2) = smoothstep_down(x, rhoE_post, rhoE_pre, 1/1000);
 %     u0 = [rhoL(:) + rhoR(:); rhouL + rhouR; rhoEL + rhoER];
 end
 
-function sw = sourcew(u, q, w, v, x, t, mu, eta)
-    nspecies = 5;
-
-    sw = sym(zeros(nspecies + 2,1));
-    rho_i = sym(zeros(5,1));
-    rho = sym(0);
-
-    % Conservative Variables
-    for ispecies = 1:nspecies
-        rho_i(ispecies) = u(ispecies); %subspecies density
-        rho = rho + rho_i(ispecies); %total mixture density
-    end
-
-    rhou = u(nspecies+1);
-    rhoE = u(nspecies+2);
-
-    rhoinv = 1.0 / rho;
-    u = rhou * rhoinv; %velocity
-    E = rhoE*rhoinv; %energy
-
-    % Mutation inputs
-    rhoe = rhoE - 0.5 * rho * u^2;
-    for i = 1:nspecies
-        sw(i) = rho_i(i);
-    end
-    sw(nspecies + 1) = rhoe;
-end
-
-function w0 = initw(x, mu, eta)
-    nspecies = 5;
-    w0 = sym(zeros(nspecies + 1,1));
-end
+% function sw = sourcew(u, q, w, v, x, t, mu, eta)
+%     nspecies = 5;
+% 
+%     sw = sym(zeros(nspecies + 2,1));
+%     rho_i = sym(zeros(5,1));
+%     rho = sym(0);
+% 
+%     % Conservative Variables
+%     for ispecies = 1:nspecies
+%         rho_i(ispecies) = u(ispecies); %subspecies density
+%         rho = rho + rho_i(ispecies); %total mixture density
+%     end
+% 
+%     rhou = u(nspecies+1);
+%     rhoE = u(nspecies+2);
+% 
+%     rhoinv = 1.0 / rho;
+%     u = rhou * rhoinv; %velocity
+%     E = rhoE*rhoinv; %energy
+% 
+%     % Mutation inputs
+%     rhoe = rhoE - 0.5 * rho * u^2;
+%     for i = 1:nspecies
+%         sw(i) = rho_i(i);
+%     end
+%     sw(nspecies + 1) = rhoe;
+% end
+% 
+% function w0 = initw(x, mu, eta)
+%     nspecies = 5;
+%     w0 = sym(zeros(nspecies + 1,1));
+% end
