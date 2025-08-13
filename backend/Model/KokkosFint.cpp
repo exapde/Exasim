0e3e55283412b0d66b2252aa09f2cb209e266881
exapde <exapde@gmail.com>
2025-08-13 08:22:47 -0400

    Apply .gitignore (stop tracking ignored files)

--- backend/Model/KokkosFint.cpp
+++
@@ -1,6 +0,0 @@
-void KokkosFint(dstype* f, const dstype* xdg, const dstype* udg, const dstype* odg, const dstype* wdg,
-             const dstype* uhg, const dstype* nlg, const dstype* tau, const dstype* uinf, const dstype* param, const dstype time,
-             const int modelnumber, const int ib, const int ng, const int nc, const int ncu, const int nd, const int ncx,
-             const int nco, const int ncw)
-{
-}
