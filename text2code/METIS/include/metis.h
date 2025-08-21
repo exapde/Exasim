ea231de35c6009ce093e60d3b90cdafbc626c423
Ngoc-Cuong Nguyen <70904303+exapde@users.noreply.github.com>
2025-08-21 00:20:24 -0400

    update makefile

--- text2code/METIS/include/metis.h
+++ text2code/METIS/include/metis.h
@@ -8,6 +8,8 @@
 #define REALTYPEWIDTH 32
 #define IDXTYPEWIDTH 32
 #define REALTYPEWIDTH 32
+#define IDXTYPEWIDTH 32
+#define REALTYPEWIDTH 32
 /*!
 \file metis.h 
 \brief This file contains function prototypes and constant definitions for METIS
