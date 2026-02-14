d9c3c90681dc90f7aea96fa19a05ed328f34a72f
Ngoc-Cuong Nguyen <70904303+exapde@users.noreply.github.com>
2026-02-14 15:58:25 -0500

    gitignore: stop tracking backend model generated kernels

--- backend/Model/KokkosInitodg2.cpp
+++
@@ -1,9 +0,0 @@
-void KokkosInitodg2(dstype* f, const dstype* xdg, const dstype* uinf, const dstype* param, const int modelnumber, const int ng, const int ncx, const int nce, const int npe, const int ne)
-{
-	Kokkos::parallel_for("Initodg2", ng, KOKKOS_LAMBDA(const size_t i) {
-		int j = i%npe;
-		int k = i/npe;
-		f[j+npe*0+npe*nce*k] = 0.0;
-	});
-}
-
